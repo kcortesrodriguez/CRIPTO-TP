09cd97d8743eb675b7a86ba87f47de98475084cd
j1nma <j1nma@users.noreply.github.com>
2019-06-03 18:08:36 -0300

    random for urandom

---
+++ random.c
@@ -0,0 +1,33 @@
+//
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "random.h"
+
+int urandom_fd = -2;
+
+void urandom_init() {
+    urandom_fd = open("/dev/urandom", O_RDONLY);
+
+    if (urandom_fd == -1) {
+        int errsv = urandom_fd;
+        printf("Error opening [/dev/urandom]: %i\n", errsv);
+        exit(1);
+    }
+}
+
+unsigned long urandom() {
+    unsigned long buf_impl;
+    unsigned long *buf = &buf_impl;
+
+    if (urandom_fd == -2) {
+        urandom_init();
+    }
+
+    /* Read 4 bytes, or 32 bits into *buf, which points to buf_impl */
+    read(urandom_fd, buf, sizeof(long));
+    return buf_impl;
+}
+
---
+++ random.h
@@ -0,0 +1,9 @@
+//
+//
+
+#ifndef CRIPTO_TP_RANDOM_H
+#define CRIPTO_TP_RANDOM_H
+
+unsigned long urandom();
+
+#endif //CRIPTO_TP_RANDOM_H
