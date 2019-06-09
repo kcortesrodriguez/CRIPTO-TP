34e170dfbdcf7324d619810341a9ad34dc268239
j1nma <j1nma@users.noreply.github.com>
2019-06-09 06:42:27 -0300

    projectionSd, remainderR

--- distribution.h
+++ distribution.h
@@ -1,9 +1,10 @@
-//
-//
-
 #ifndef CRIPTO_TP_DISTRIBUTION_H
 #define CRIPTO_TP_DISTRIBUTION_H
 
 int **matA(int n, int k);
+
+int **projectionSd(int **A, int n, int k, int inverses[251]);
+
+int **remainderR(int **secretS, int **projectionSd, int n);
 
 #endif //CRIPTO_TP_DISTRIBUTION_H
