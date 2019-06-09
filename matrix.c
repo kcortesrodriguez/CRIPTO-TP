ec2ed88bfe6bcf0cbbe69a722e38f920d79d9b57
j1nma <j1nma@users.noreply.github.com>
2019-06-09 06:42:14 -0300

    inverse, add, subtract, freeMatrix

--- matrix.c
+++ matrix.c
@@ -1,8 +1,7 @@
-//
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrix.h"
+#include "modular.h"
 
 // Function to get cofactor of mat[p][q] in temp[][]. n is current
 // dimension of mat[][]
@@ -17,8 +16,7 @@ void getCofactor(int **mat, int **temp, int p, int q, int n) {
             if (row != p && col != q) {
                 temp[i][j++] = mat[row][col];
 
-                // Row is filled, so increase row index and
-                // reset col index
+                // Row is filled, so increase row index and reset col index
                 if (j == n - 1) {
                     j = 0;
                     i++;
@@ -28,8 +26,8 @@ void getCofactor(int **mat, int **temp, int p, int q, int n) {
     }
 }
 
-/* Recursive function for finding determinant of matrix.
-   n is current dimension of mat[][], N is the original dimension of the matrix. */
+// Recursive function for finding determinant of matrix.
+// n is current dimension of mat[][], N is the original dimension of the matrix.
 int determinantOfMatrix(int **mat, int N, int n) {
     int D = 0; // Initialize result
 
@@ -38,8 +36,8 @@ int determinantOfMatrix(int **mat, int N, int n) {
         return mat[0][0];
 
     // To store cofactors
-    int **temp = (int **) malloc(N * sizeof(int *)); //TODO free
-    for (int i = 0; i < N; i++) temp[i] = (int *) malloc(N * sizeof(int));
+    int **temp = (int **) malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++) temp[i] = (int *) calloc((size_t) N, sizeof(int));
 
     int sign = 1;  // To store sign multiplier
 
@@ -52,6 +50,8 @@ int determinantOfMatrix(int **mat, int N, int n) {
         // terms are to be added with alternate sign
         sign = -sign;
     }
+
+    freeMatrix(temp, n);
 
     return D;
 }
@@ -75,8 +75,102 @@ void multiply(int **mat1, int **mat2, int **res, int n, int m, int k) {
         for (j = 0; j < m; j++) {
             res[i][j] = 0;
             for (h = 0; h < k; h++)
-                res[i][j] += mat1[i][h] * mat2[h][j];
-            res[i][j] = res[i][j] % 251;
+                res[i][j] = modulo(res[i][j] + modulo(mat1[i][h] * mat2[h][j], 251), 251);
         }
     }
+}
+
+// Function to perform the inverse operation on the matrix.
+void inverse(int **m, int **mInverse, int n, int inverses[251]) {
+
+    printf("\n");
+    printf("m matrix:\n");
+    for (int row = 0; row < n; row++) {
+        for (int columns = 0; columns < n; columns++) {
+            printf("  %d", m[row][columns]);
+        }
+        printf("\n");
+    }
+
+    // Create the augmented matrix
+    // Add the identity matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2 * n; j++) {
+
+            if (j < n)
+                mInverse[i][j] = m[i][j];
+
+            // Add '1' at the diagonal places
+            if (j == (i + n))
+                mInverse[i][j] = 1;
+        }
+    }
+
+    printf("\n");
+    printf("Aug plus id matrix:\n");
+    for (int row = 0; row < n; row++) {
+        for (int columns = 0; columns < 2 * n; columns++) {
+            printf("  %d", mInverse[row][columns]);
+        }
+        printf("\n");
+    }
+
+    // Interchange the row of matrix, starting from the last row
+    for (int i = n - 1; i > 0; i--) {
+
+        // Swapping the rows
+        if (mInverse[i - 1][0] < mInverse[i][0]) {
+            int *temp = mInverse[i];
+            mInverse[i] = mInverse[i - 1];
+            mInverse[i - 1] = temp;
+        }
+    }
+
+    int temp;
+
+    // Replace a row by sum of itself and a
+    // constant multiple of another row of the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                temp = modulo(mInverse[j][i] * inverses[mInverse[i][i]], 251);
+                for (int k = 0; k < 2 * n; k++) {
+                    mInverse[j][k] = modulo(mInverse[j][k] - modulo(mInverse[i][k] * temp, 251), 251);
+                }
+            }
+        }
+    }
+
+    // Multiply each row by a nonzero integer.
+    // Divide row element by the diagonal element
+    for (int i = 0; i < n; i++) {
+        temp = mInverse[i][i];
+        for (int j = 0; j < 2 * n; j++) {
+            mInverse[i][j] = modulo(mInverse[i][j] * inverses[temp], 251);
+        }
+    }
+}
+
+void add(int **mat1, int **mat2, int **res, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            res[i][j] = modulo(mat1[i][j] + mat2[i][j], 251);
+        }
+    }
+}
+
+void subtract(int **mat1, int **mat2, int **res, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            res[i][j] = modulo(mat1[i][j] - mat2[i][j], 251);
+        }
+    }
+}
+
+void freeMatrix(int **m, int n) {
+    for (int i = 0; i < n; i++) {
+        int *currentIntPtr = m[i];
+        free(currentIntPtr);
+    }
+    free(m);
 }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,3 @@
-//
-//
-
 #ifndef CRIPTO_TP_MATRIX_H
 #define CRIPTO_TP_MATRIX_H
 
@@ -9,5 +6,13 @@ int determinantOfMatrix(int **mat, int N, int n);
 void transpose(int **A, int **B, int n, int k);
 
 void multiply(int **mat1, int **mat2, int **res, int n, int m, int k);
+
+void inverse(int **m, int **mInverse, int n, int inverses[251]);
+
+void add(int **mat1, int **mat2, int **res, int n);
+
+void subtract(int **mat1, int **mat2, int **res, int n);
+
+void freeMatrix(int **m, int n);
 
 #endif //CRIPTO_TP_MATRIX_H
